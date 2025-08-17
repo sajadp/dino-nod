c47ba353ca0b9a36e5fd3126b19f915c524cbd32
sajadp <bg.alborz@gmail.com>
2025-08-17 03:31:03 +0330

    fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      define: {
+      base: '/dino-nod/', // 👈 این خط رو اضافه کن
+	  define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
